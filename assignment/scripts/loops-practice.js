48719b042052534868f7211764d552d1b9ed3002
Leah Grim <grimchelate@gmail.com>
2021-11-14 00:53:30 -0600

    updated changes to github

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -41,6 +41,7 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
+let i=0
 for (i of stars){
   console.log(i);
 }
@@ -50,9 +51,10 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-console.log('Some stars:');
-for (let i=0; i< stars.length; i++){
+i = 0;
+while (i < stars.length) {
   console.log(stars[i]);
+  i++;
 }
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 let index = 0;
